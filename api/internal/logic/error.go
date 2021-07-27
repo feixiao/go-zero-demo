37a7192d8a3c1778a4002d7fc9ea8e81b38c7c64
feixiao <feixiao2020@sina.com>
2021-07-27 16:46:39 +0800

    支持http自定义错误信息

---
+++ api/internal/logic/error.go
@@ -0,0 +1,30 @@
+package logic
+
+type (
+	Error struct {
+		Code int    `json:"code"`
+		Msg  string `json:"msg"`
+	}
+)
+
+func NewError(code int, msg string) *Error {
+	err := &Error{Code: code, Msg: msg}
+	return err
+}
+
+func (e *Error) Error() string {
+	return e.Msg
+}
+
+type ErrorResponse struct {
+	Code int    `json:"code"`
+	Msg  string `json:"message"`
+}
+
+// Data 为了支持http自定义错误信息 https://go-zero.dev/cn/error-handle.html
+func (e *Error) Data() *ErrorResponse {
+	return &ErrorResponse{
+		Code: e.Code,
+		Msg:  e.Msg,
+	}
+}
