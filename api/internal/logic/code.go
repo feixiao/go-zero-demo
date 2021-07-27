d4e108d322a2bfea9986be659863ce18404bf05b
feixiao <feixiao2020@sina.com>
2021-07-27 16:47:32 +0800

    添加错误码

---
+++ api/internal/logic/code.go
@@ -0,0 +1,14 @@
+package logic
+
+var (
+
+	// [1000:1100)
+	ErrInternalServer = NewError(1000, "服务内部错误")
+
+	// User [1100:1200)
+	ErrDuplicateUsername  = NewError(1100, "用户已经注册")
+	ErrDuplicateMobile    = NewError(1101, "手机号已经被占用")
+	ErrUsernameUnRegister = NewError(1102, "用户未注册")
+	ErrIncorrectPassword  = NewError(1103, "用户密码错误")
+	ErrUserNotFound       = NewError(1104, "用户不存在")
+)
