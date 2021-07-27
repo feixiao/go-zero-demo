9106c573afa14a160f74dc620a7cb9c2ac3247bd
feixiao <feixiao2020@sina.com>
2021-07-27 16:30:06 +0800

    1: proto文件需要不同服务不同，否者会冲突 2: 自动化生成数据库操作代码

--- rpc/internal/svc/servicecontext.go
+++ rpc/internal/svc/servicecontext.go
@@ -1,13 +1,22 @@
 package svc
 
-import "github.com/feixiao/go-zero-demo/rpc/internal/config"
+import (
+	"github.com/feixiao/go-zero-demo/rpc/internal/config"
+	"github.com/feixiao/go-zero-demo/rpc/model"
+	"github.com/tal-tech/go-zero/core/stores/sqlx"
+)
 
 type ServiceContext struct {
 	Config config.Config
+
+	UserModel model.TbUsersModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	conn := sqlx.NewMysql(c.Mysql.DataSource)
+	userModel := model.NewTbUsersModel(conn, c.CacheRedis)
 	return &ServiceContext{
-		Config: c,
+		Config:    c,
+		UserModel: userModel,
 	}
 }
